3a96c84eb10cf520b8ce94a5a0ec1ddffae19311
helin <helin@sodo-tech.com>
2016-04-23 17:50:31 +0800

    添加对xml的解析

--- src/com/ihelin/io/ObjectSerializableDemo.java
+++ src/com/ihelin/io/ObjectSerializableDemo.java
@@ -1,10 +1,8 @@
 package com.ihelin.io;
 
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 
 public class ObjectSerializableDemo {
 	
