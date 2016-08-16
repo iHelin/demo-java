5e37b38ab150cddd10f49a00e5301ff0473bcb7c
iHelin <ihelin@outlook.com>
2016-08-16 13:42:49 +0800

    update

--- src/com/ihelin/test/Person.java
+++ src/com/ihelin/test/Person.java
@@ -1,20 +1,13 @@
 package com.ihelin.test;
 
-import java.util.HashMap;
-import java.util.Map;
+public class Person implements java.io.Serializable {
 
-public class Person {
-	public static void main(String[] args) {
-		Map<String,Object> proMap = new HashMap<String,Object>(); 
-		proMap.put("a", 1);
-		proMap.put("b", 2);
-		proMap.put("c", 3);
-		proMap.put("c", 4);
-		System.out.println(proMap.get("c"));
-	}
-	
-	public String prt(){
-		return "fewf";
-				
+	public String name;
+	public String address;
+	public transient int SSN;
+	public int number;
+
+	public void mailCheck() {
+		System.out.println("Mailing a check to " + name + " " + address);
 	}
 }
--- src/com/ihelin/xml/DOMTest.java
+++ src/com/ihelin/xml/DOMTest.java
@@ -1,6 +1,5 @@
 package com.ihelin.xml;
 
-import java.awt.print.Book;
 import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -9,7 +8,6 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
