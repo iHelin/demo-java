ea43d71e44f8dc3a695e10969b9331fe58ee3d05
iHelin <ihelin@outlook.com>
2018-10-21 09:36:36 +0800

    update

--- src/main/java/me/ianhe/core/Base.java
+++ src/main/java/me/ianhe/core/Base.java
@@ -1,5 +1,7 @@
 package me.ianhe.core;
 
+import java.math.BigDecimal;
+
 /**
  * @author iHelin
  * @date 2018/10/20 10:51
@@ -16,5 +18,8 @@ public class Base {
         System.out.println(Double.NaN);
         //从Java 7开始。可以为数字字面量加下划线
         System.out.println(1_224_134);
+        System.out.println(2.0 - 1.9);
+        System.out.println(BigDecimal.valueOf(2.0).subtract(BigDecimal.valueOf(1.9)));
+        System.out.println(new BigDecimal("2.0").subtract(new BigDecimal("1.9")));
     }
 }
---
+++ src/main/java/me/ianhe/core/StrictfpTest.java
@@ -0,0 +1,15 @@
+package me.ianhe.core;
+
+/**
+ * 使用strictfp关键字精确计算
+ *
+ * @author iHelin
+ * @date 2018/10/20 11:19
+ */
+public class StrictfpTest {
+
+    public static strictfp void main(String[] args) {
+        double w = Double.MAX_VALUE * Integer.MAX_VALUE / Integer.MAX_VALUE;
+        System.out.println(w);
+    }
+}
