30f8dad22d5640ff8a6a355f25c824e44a4c91c8
iHelin <ihelin@outlook.com>
2020-12-20 18:10:48 +0800

    添加flink测试

---
+++ src/main/java/me/ianhe/flink/StreamWorkCount.java
@@ -0,0 +1,26 @@
+package me.ianhe.flink;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class StreamWorkCount {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        String inputPath = "/Users/iHelin/Documents/IdeaProjects/hello/src/main/resources/flink-text.txt";
+
+        DataStreamSource<String> source = environment.readTextFile(inputPath);
+
+        SingleOutputStreamOperator<Tuple2<String, Integer>> result = source.flatMap(new MyFlatMapper())
+                .keyBy(0)
+                .sum(1);
+
+        result.print();
+
+        environment.execute();
+
+    }
+}
