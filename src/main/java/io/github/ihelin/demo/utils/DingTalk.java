a44f57a7547c9a08ed0f70aa40f7f5c7fc1530ff
iHelin <ihelin@outlook.com>
2021-01-08 19:04:09 +0800

    update

--- src/main/java/io/github/ihelin/demo/utils/DingTalk.java
+++ src/main/java/io/github/ihelin/demo/utils/DingTalk.java
@@ -1,21 +1,17 @@
 package io.github.ihelin.demo.utils;
 
 import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,16 +23,17 @@ public class DingTalk {
 
     private static final String SIGN = "SECb84365c590c77868c69f7bdac8600759ff457301166e672078b59b383d609b8d";
     private static final String TOKEN = "5c48f9fe08ba0af65c08def326421f0fd1c54de1e97652d63cb3a05bace2641d";
-    private static final String URL = "https://oapi.dingtalk.com/robot/send?access_token={1}&timestamp={2}&sign={3}";
+    private static final String URL = "https://oapi.dingtalk.com/robot/send?access_token={access_token}&sign={sign}&timestamp={timestamp}";
+    private static final Logger LOGGER = LoggerFactory.getLogger(DingTalk.class);
 
     private DingTalk() {
     }
 
-    public static void main(String[] args) throws MalformedURLException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
-        DingTalk.say("hello");
+    public static void main(String[] args) {
+        System.out.println(DingTalk.say("hello"));
     }
 
-    public static String say(String words) throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
+    public static String say(String words) {
         Long timestamp = System.currentTimeMillis();
         String sign = sign();
 
@@ -53,19 +50,27 @@ public class DingTalk {
 
         HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
 
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity(URL, request, String.class, TOKEN, timestamp, sign);
-        return responseEntity.getBody();
+        String result = restTemplate.postForObject(URL,
+            request,
+            String.class,
+            TOKEN, sign, timestamp);
+        return result;
     }
 
 
-    private static String sign() throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
+    private static String sign() {
         Long timestamp = System.currentTimeMillis();
         String stringToSign = timestamp + "\n" + SIGN;
         byte[] bytesToSign = stringToSign.getBytes(StandardCharsets.UTF_8);
-        Mac mac = Mac.getInstance("HmacSHA256");
-        mac.init(new SecretKeySpec(SIGN.getBytes(StandardCharsets.UTF_8), "HmacSHA256"));
-        byte[] signData = mac.doFinal(bytesToSign);
-        String sign = URLEncoder.encode(new String(Base64.encodeBase64(signData)), "UTF-8");
+        String sign = "";
+        try {
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(new SecretKeySpec(SIGN.getBytes(StandardCharsets.UTF_8), "HmacSHA256"));
+            byte[] signData = mac.doFinal(bytesToSign);
+            sign = URLEncoder.encode(new String(Base64.encodeBase64(signData)), "UTF-8");
+        } catch (Exception e) {
+            LOGGER.error("签名失败", e);
+        }
         return sign;
     }
 }
